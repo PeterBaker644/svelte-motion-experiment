acbb1c484c8efb48727105c69fb89e24a943e4a0
Blackstar <baker644@umn.edu>
2021-04-19 16:53:38 -0500

    Broke out card into a component

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,7 @@
   // This export pulls in the name from main.js
 	// export let name;
   import { spring } from 'svelte/motion';
-
-  let card = {
-    width:200,
-    height:280
-  };
+  import Card from "./components/Card.svelte";
 	let coords = spring({ x: 50, y: 50 }, {
 		stiffness: 0.1,
 		damping: 0.25
@@ -19,18 +15,7 @@
   on:mousedown="{() => size.set(30)}"
   on:mouseup="{() => size.set(10)}"
 >
-  <div class="card" style="
-    transform:translate({$coords.x - card.width/2}px,{$coords.y - card.height/2}px);
-    height:{card.height}px;
-    width:{card.width}px;
-    ">
-    <h2>Monster Name</h2>
-    <div class="image">Fake Image</div>
-    <p class="details">STR:</p>
-    <p class="details">WK:</p>
-    <p class="details">PWR:</p>
-    <p>coords: {Math.round($coords.x)},{Math.round($coords.y)}</p>
-  </div>
+  <Card {coords}/>
 
 	<h1>Svelte Motion Test</h1>
 	<p>This is a test webpage.</p>
@@ -64,33 +49,6 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
-
-  h2,p {
-    margin: 0em;
-  }
-
-	.card { 
-    position:absolute; 
-    color: #ff3e00;
-    border: 0.2em solid black;
-    border-radius: 0.4em;
-    background-color: rgba(255, 255, 255, 0.7); 
-    display: grid;
-    gap: 0;
-    align-items: center;
-  }
-
-  .details {
-    margin: 0em;
-  }
-
-  .image {
-    width: 150px;
-    height: 100px;
-    margin: 0.2em 0em;
-    border: 1px solid black;
-    justify-self: center;
-  }
 
 	@media (min-width: 640px) {
 		main {
---
+++ src/components/Card.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let coords;
+  let card = {
+    width:200,
+    height:280
+  };
+</script>
+
+<div class="card" style="
+transform:translate({$coords.x - card.width/2}px,{$coords.y - card.height/2}px);
+height:{card.height}px;
+width:{card.width}px;
+">
+  <h2>Monster Name</h2>
+  <div class="image">Fake Image</div>
+  <p class="details">STR:</p>
+  <p class="details">WK:</p>
+  <p class="details">PWR:</p>
+  <p>coords: {Math.round($coords.x)},{Math.round($coords.y)}</p>
+</div>
+
+<style>
+
+  h2,p {
+    margin: 0em;
+  }
+  
+  .card {
+    position: absolute;
+    color: #ff3e00;
+    border: 0.2em solid black;
+    border-radius: 0.4em;
+    background-color: rgba(255, 255, 255, 0.7);
+    display: grid;
+    gap: 0;
+    align-items: center;
+  }
+
+  .details {
+    margin: 0em;
+  }
+
+  .image {
+    width: 150px;
+    height: 100px;
+    margin: 0.2em 0em;
+    border: 1px solid black;
+    justify-self: center;
+  }
+</style>
